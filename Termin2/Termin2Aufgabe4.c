4c0c4c3cbd1b294e9b4a815e0bad2dfc275ecdc2
Marco Hitsch <marco.hitsch@gmail.com>
2018-11-08 11:02:45 +0100

    A4
    Signed-off-by: Marco Hitsch <marco.hitsch@gmail.com>

--- Termin2/Termin2Aufgabe4.c
+++ Termin2/Termin2Aufgabe4.c
@@ -18,6 +18,16 @@ void taste_irq_handler (void)
 
 int main(void)
 {
-
+	StructAIC* aicbase = AIC_BASE; // Basisadresse AIC
+	aicbase->AIC_IDCR = 1; // Interrupt PIOB ausschalten
+	aicbase->AIC_ICCR = 1; // Interrupt PIOB löschen
+	aicbase->AIC_SVR[1] = (unsigned int)irq_handler;
+	aicbase->AIC_SMR[1] = 0x7; // Priorität
+	aicbase->AIC_IECR = 1;// Interrupt PIOB einschalten
+	piobaseB->PIO_IER = ...; // Interrupt innerhalb PIOB erlauben
+	while( 1 ); // Endlosschleife
+	aicbase->AIC_IDCR = 1 << 14; // Interrupt PIOB ausschalten
+	aicbase->AIC_ICCR = 1 << 14; // Interrupt PIOB löschen
+	
 	return 0;
 }
